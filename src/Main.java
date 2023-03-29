cd38e66606057df294bf6f8687cb9432cf64bb80
natagusina <natagusina@gmail.com>
2023-03-29 14:52:47 +0100

    issues in sun still resolving

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
        solar.sun="1";
        solar.moon="2";
        solar.planets="2";
+
+       sun fero=new sun;
+       fero.color="red";
+       fero.temperature=34234;
+       fero.heat="3243243243k";
        
     }
 }
